00796245211d9cb57f0a365374a0e0ce7b82edf6
Avishek-Ad <adhikariavishek03@gmail.com>
2026-07-21 22:20:03 +0545

    fix: Update Swagger UI paths in URL configurations for admin, audit, and user services

--- admin_service/core/urls.py
+++ admin_service/core/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
-    SpectacularRedocView,
 )
 
 urlpatterns = [
@@ -29,8 +28,5 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     
     # 2. Swagger UI
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    
-    # 3. ReDoc UI (Optional alternative layout)
-    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url='/api/admin/schema/'), name='swagger-ui'),
 ]
