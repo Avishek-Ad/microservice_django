77a761f0997a73118d2dde6d77f3f3b776af212a
Avishek-Ad <adhikariavishek03@gmail.com>
2026-07-24 15:54:03 +0545

    feat: Add CareerPage component with embedded recruitment script

---
+++ frontend/app/career/page.tsx
@@ -0,0 +1,11 @@
+export default function CareerPage() {
+    return (
+        <>
+            <script
+                src="https://dev-v1.recruitablehr.com/career-embed.js?org=dc2be3e4-0e80-463c-ad63-985327d5ea9c"
+                async
+            ></script>
+            <div id="recruitable-career-page"></div>
+        </>
+    );
+}
